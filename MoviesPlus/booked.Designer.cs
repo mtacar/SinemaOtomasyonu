3f498b350187939102151c1edcc5c0f499ab23d3
fkavraz <170290032@firat.edu.tr>
2021-06-14 23:02:56 +0300

    CASE-15

---
+++ MoviesPlus/booked.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace BookMyshow
+{
+    partial class booked
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.printbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timelbl = new System.Windows.Forms.Label();
+            this.pricelbl = new System.Windows.Forms.Label();
+            this.seatlbl = new System.Windows.Forms.Label();
+            this.thelbl = new System.Windows.Forms.Label();
+            this.datelbl = new System.Windows.Forms.Label();
+            this.titlelbl = new System.Windows.Forms.Label();
+            this.tlbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(83, 89);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(833, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // printbtn
+            // 
+            this.printbtn.BackColor = System.Drawing.Color.Black;
+            this.printbtn.Location = new System.Drawing.Point(713, 370);
+            this.printbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.printbtn.Name = "printbtn";
+            this.printbtn.Size = new System.Drawing.Size(100, 28);
+            this.printbtn.TabIndex = 1;
+            this.printbtn.Text = "Yazdır";
+            this.printbtn.UseVisualStyleBackColor = false;
+            this.printbtn.Click += new System.EventHandler(this.printbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(244, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Film Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(244, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tarih:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(244, 143);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Salon :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(244, 180);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Koltuk:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(244, 32);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Bilet Id :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(244, 217);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Ücret:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(456, 106);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Saat:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.timelbl);
+            this.groupBox1.Controls.Add(this.pricelbl);
+            this.groupBox1.Controls.Add(this.seatlbl);
+            this.groupBox1.Controls.Add(this.thelbl);
+            this.groupBox1.Controls.Add(this.datelbl);
+            this.groupBox1.Controls.Add(this.titlelbl);
+            this.groupBox1.Controls.Add(this.tlbl);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(21, 31);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(792, 266);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // timelbl
+            // 
+            this.timelbl.AutoSize = true;
+            this.timelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timelbl.Location = new System.Drawing.Point(527, 106);
+            this.timelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timelbl.Name = "timelbl";
+            this.timelbl.Size = new System.Drawing.Size(0, 20);
+            this.timelbl.TabIndex = 16;
+            // 
+            // pricelbl
+            // 
+            this.pricelbl.AutoSize = true;
+            this.pricelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pricelbl.Location = new System.Drawing.Point(344, 217);
+            this.pricelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.pricelbl.Name = "pricelbl";
+            this.pricelbl.Size = new System.Drawing.Size(0, 20);
+            this.pricelbl.TabIndex = 15;
+            // 
+            // seatlbl
+            // 
+            this.seatlbl.AutoSize = true;
+            this.seatlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seatlbl.Location = new System.Drawing.Point(344, 180);
+            this.seatlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.seatlbl.Name = "seatlbl";
+            this.seatlbl.Size = new System.Drawing.Size(0, 20);
+            this.seatlbl.TabIndex = 14;
+            // 
+            // thelbl
+            // 
+            this.thelbl.AutoSize = true;
+            this.thelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thelbl.Location = new System.Drawing.Point(344, 143);
+            this.thelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.thelbl.Name = "thelbl";
+            this.thelbl.Size = new System.Drawing.Size(0, 20);
+            this.thelbl.TabIndex = 13;
+            this.thelbl.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // datelbl
+            // 
+            this.datelbl.AutoSize = true;
+            this.datelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datelbl.Location = new System.Drawing.Point(344, 106);
+            this.datelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.datelbl.Name = "datelbl";
+            this.datelbl.Size = new System.Drawing.Size(0, 20);
+            this.datelbl.TabIndex = 12;
+            // 
+            // titlelbl
+            // 
+            this.titlelbl.AutoSize = true;
+            this.titlelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titlelbl.Location = new System.Drawing.Point(344, 69);
+            this.titlelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titlelbl.Name = "titlelbl";
+            this.titlelbl.Size = new System.Drawing.Size(0, 20);
+            this.titlelbl.TabIndex = 11;
+            // 
+            // tlbl
+            // 
+            this.tlbl.AutoSize = true;
+            this.tlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tlbl.Location = new System.Drawing.Point(344, 32);
+            this.tlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.tlbl.Name = "tlbl";
+            this.tlbl.Size = new System.Drawing.Size(0, 20);
+            this.tlbl.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = global::MoviesPlus.Properties.Resources.Loading_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 246);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.printbtn);
+            this.groupBox2.Location = new System.Drawing.Point(83, 177);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(835, 431);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // booked
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1005, 697);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "booked";
+            this.Text = "booked";
+            this.Load += new System.EventHandler(this.booked_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button printbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label thelbl;
+        private System.Windows.Forms.Label datelbl;
+        private System.Windows.Forms.Label titlelbl;
+        private System.Windows.Forms.Label tlbl;
+        private System.Windows.Forms.Label timelbl;
+        private System.Windows.Forms.Label pricelbl;
+        private System.Windows.Forms.Label seatlbl;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ MoviesPlus/booked.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net; 
+
+namespace BookMyshow
+{
+    public partial class booked : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source='DESKTOP-RPQ1FVH\\SQLEXPRESS';initial catalog= bookshow;integrated Security=true");
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        SqlDataAdapter da1 = new SqlDataAdapter();
+        DataSet ds1 = new DataSet();
+        public System.Drawing.Image img;
+        public static int i=0;
+        public booked()
+        {
+            InitializeComponent();
+        }
+
+        private void booked_Load(object sender, EventArgs e)
+        {
+            if (log.username != String.Empty)
+            {
+                try
+                {
+                    da.SelectCommand = new SqlCommand("select * from booked where uid= '"+log.username+"' order by (id) DESC", cs);
+                    ds.Clear();
+                    da.Fill(ds);
+                    comboBox1.DataSource = ds.Tables[0];
+                    comboBox1.DisplayMember = "id";
+                    this.backgroundWorker1.DoWork += BackgroundWorker1_DoWork;
+
+                }
+                catch
+                {
+
+
+                }
+            }else
+            {
+                groupBox2.Visible = false;
+            }
+
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void printbtn_Click(object sender, EventArgs e)
+        {
+            printtik p = new printtik();
+            p.print(pictureBox1.Image, tlbl.Text, titlelbl.Text, datelbl.Text, timelbl.Text, pricelbl.Text, thelbl.Text, seatlbl.Text);
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox1.ImageLocation = ds.Tables[0].Rows[comboBox1.SelectedIndex][3].ToString();
+                //WebClient wc = new WebClient();
+               // byte[] bytes = wc.DownloadData(ds.Tables[0].Rows[comboBox1.SelectedIndex][3].ToString());
+               // MemoryStream ms = new MemoryStream(bytes);
+               // this.img = System.Drawing.Image.FromStream(ms);
+               // pictureBox1.Image = img;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+           
+
+            tlbl.Text = ds.Tables[0].Rows[comboBox1.SelectedIndex][0].ToString();
+            titlelbl.Text = ds.Tables[0].Rows[comboBox1.SelectedIndex][2].ToString() ;
+
+           
+
+            datelbl.Text = ds.Tables[0].Rows[comboBox1.SelectedIndex][4].ToString() ;
+            timelbl.Text = ds.Tables[0].Rows[comboBox1.SelectedIndex][5].ToString() ;
+            pricelbl.Text = ds.Tables[0].Rows[comboBox1.SelectedIndex][6].ToString() ;
+            thelbl.Text = ds.Tables[0].Rows[comboBox1.SelectedIndex][7].ToString() ;
+            seatlbl.Text = ds.Tables[0].Rows[comboBox1.SelectedIndex][8].ToString() ;
+
+            
+            
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            WebClient wc = new WebClient();
+            byte[] bytes = wc.DownloadData(ds.Tables[0].Rows[comboBox1.SelectedIndex][3].ToString());
+            MemoryStream ms = new MemoryStream(bytes);
+            this.img = System.Drawing.Image.FromStream(ms);
+            pictureBox1.Image = img;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            i = 1;
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
