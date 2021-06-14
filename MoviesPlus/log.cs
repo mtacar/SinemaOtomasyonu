4facd9c86a91946e9b80ab0a247841a02ac3fe56
fkavraz <170290032@firat.edu.tr>
2021-06-14 22:41:42 +0300

    CASE-6

---
+++ MoviesPlus/log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMyshow
+{
+    public class log
+    {
+        public static String username { get; set; }
+        public static String pwd { get; set; }
+    }
+}
