d5b35d7114801ef86694281801c3e12cb2b8a6a7
fkavraz <170290032@firat.edu.tr>
2021-06-14 22:38:50 +0300

    CASE-2 ve CASE-3

---
+++ MoviesPlus/signinpg.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace BookMyshow
+{
+    partial class signinpg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(signinpg));
+            this.loggrbx = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.registerbtnr = new System.Windows.Forms.Button();
+            this.signuplblr = new System.Windows.Forms.Label();
+            this.signinbtnr = new System.Windows.Forms.Button();
+            this.pwdtxtr = new System.Windows.Forms.TextBox();
+            this.usertxtr = new System.Windows.Forms.TextBox();
+            this.pwdbtnr = new System.Windows.Forms.Label();
+            this.userbtnr = new System.Windows.Forms.Label();
+            this.loggrbx.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loggrbx
+            // 
+            this.loggrbx.BackColor = System.Drawing.Color.Transparent;
+            this.loggrbx.Controls.Add(this.button1);
+            this.loggrbx.Controls.Add(this.registerbtnr);
+            this.loggrbx.Controls.Add(this.signuplblr);
+            this.loggrbx.Controls.Add(this.signinbtnr);
+            this.loggrbx.Controls.Add(this.pwdtxtr);
+            this.loggrbx.Controls.Add(this.usertxtr);
+            this.loggrbx.Controls.Add(this.pwdbtnr);
+            this.loggrbx.Controls.Add(this.userbtnr);
+            resources.ApplyResources(this.loggrbx, "loggrbx");
+            this.loggrbx.Name = "loggrbx";
+            this.loggrbx.TabStop = false;
+            this.loggrbx.Enter += new System.EventHandler(this.loggrbx_Enter);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // registerbtnr
+            // 
+            this.registerbtnr.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.registerbtnr, "registerbtnr");
+            this.registerbtnr.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.registerbtnr.Name = "registerbtnr";
+            this.registerbtnr.UseVisualStyleBackColor = false;
+            this.registerbtnr.Click += new System.EventHandler(this.registerbtnr_Click);
+            // 
+            // signuplblr
+            // 
+            resources.ApplyResources(this.signuplblr, "signuplblr");
+            this.signuplblr.BackColor = System.Drawing.Color.Transparent;
+            this.signuplblr.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.signuplblr.Name = "signuplblr";
+            this.signuplblr.Click += new System.EventHandler(this.signuplblr_Click);
+            // 
+            // signinbtnr
+            // 
+            this.signinbtnr.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.signinbtnr, "signinbtnr");
+            this.signinbtnr.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.signinbtnr.Name = "signinbtnr";
+            this.signinbtnr.UseVisualStyleBackColor = false;
+            this.signinbtnr.Click += new System.EventHandler(this.signinbtnr_Click);
+            // 
+            // pwdtxtr
+            // 
+            this.pwdtxtr.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            resources.ApplyResources(this.pwdtxtr, "pwdtxtr");
+            this.pwdtxtr.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.pwdtxtr.Name = "pwdtxtr";
+            // 
+            // usertxtr
+            // 
+            this.usertxtr.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            resources.ApplyResources(this.usertxtr, "usertxtr");
+            this.usertxtr.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.usertxtr.Name = "usertxtr";
+            this.usertxtr.Tag = "";
+            // 
+            // pwdbtnr
+            // 
+            resources.ApplyResources(this.pwdbtnr, "pwdbtnr");
+            this.pwdbtnr.BackColor = System.Drawing.Color.Transparent;
+            this.pwdbtnr.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pwdbtnr.Name = "pwdbtnr";
+            // 
+            // userbtnr
+            // 
+            resources.ApplyResources(this.userbtnr, "userbtnr");
+            this.userbtnr.BackColor = System.Drawing.Color.Transparent;
+            this.userbtnr.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.userbtnr.Name = "userbtnr";
+            // 
+            // signinpg
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.loggrbx);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "signinpg";
+            this.Load += new System.EventHandler(this.signinpg_Load);
+            this.loggrbx.ResumeLayout(false);
+            this.loggrbx.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox loggrbx;
+        private System.Windows.Forms.Button registerbtnr;
+        private System.Windows.Forms.Label signuplblr;
+        private System.Windows.Forms.Button signinbtnr;
+        private System.Windows.Forms.TextBox pwdtxtr;
+        private System.Windows.Forms.TextBox usertxtr;
+        private System.Windows.Forms.Label pwdbtnr;
+        private System.Windows.Forms.Label userbtnr;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ MoviesPlus/signinpg.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace BookMyshow
+{
+    public partial class signinpg : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source='DESKTOP-RPQ1FVH\\SQLEXPRESS';initial catalog= bookshow;integrated Security=true");
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        public signinpg()
+        {
+            InitializeComponent();
+        }
+
+        private void signinbtnr_Click(object sender, EventArgs e)
+        {
+            if (usertxtr.Text != string.Empty && pwdtxtr.Text != string.Empty)
+            {
+                da.SelectCommand = new SqlCommand("select Username,Password,type from usersdl Where Username='" + usertxtr.Text + "' and Password='" + pwdtxtr.Text + "'", cs);
+                da.Fill(ds);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0][2].ToString() == "0")
+                    {
+                        this.DialogResult = DialogResult.OK;
+                        log.username = usertxtr.Text;
+                        log.pwd = pwdtxtr.Text;
+                    }else
+                    {
+                        this.DialogResult = DialogResult.Yes;
+                        log.username = usertxtr.Text;
+                        log.pwd = pwdtxtr.Text;
+                    }
+                    foreach (Form f in Application.OpenForms)
+                    {
+                        if (f.Text == "BookM")
+                        {
+                            f.Close();
+                            Refresh();
+                            break;
+                        }
+                    }
+
+                }
+                else
+                    MessageBox.Show("Kullanıcı Adı / Şifre yanlış.Lütfen tekrar deneyin.");
+            }
+            else
+                MessageBox.Show("Lütfen Kullanıcı Adı / Şifrenizi girin.");
+        }
+
+        private void registerbtnr_Click(object sender, EventArgs e)
+        {
+            signuppg s = new signuppg();
+            this.Hide();
+
+            if (s.ShowDialog(this) == DialogResult.OK)
+            {
+                this.Activate();
+            }
+        }
+
+        private void signinpg_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = signinbtnr;
+        }
+
+        private void signuplblr_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void loggrbx_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
