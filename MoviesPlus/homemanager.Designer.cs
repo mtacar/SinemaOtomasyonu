0b26b1685f9a64aea2a30b8bf9c1e364c5bc9cdc
fkavraz <170290032@firat.edu.tr>
2021-06-14 22:59:09 +0300

    CASE-12

---
+++ MoviesPlus/homemanager.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace BookMyshow
+{
+    partial class homemanager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(homemanager));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.namebxtxt = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.urlbxtxt = new System.Windows.Forms.TextBox();
+            this.updatebtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.namebxtxt);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.urlbxtxt);
+            this.groupBox1.Controls.Add(this.updatebtn);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(-1, -9);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1005, 697);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(937, 391);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 31);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Dene";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(47, 332);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Kapak Resmi Url :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(47, 352);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(47, 185);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Yeni / Mevcut Film Başlığını Seçin:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(47, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Güncellenecek Kapağı Seçin:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // namebxtxt
+            // 
+            this.namebxtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namebxtxt.FormattingEnabled = true;
+            this.namebxtxt.Location = new System.Drawing.Point(155, 252);
+            this.namebxtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.namebxtxt.Name = "namebxtxt";
+            this.namebxtxt.Size = new System.Drawing.Size(773, 28);
+            this.namebxtxt.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "First Cover Movie",
+            "Second Cover Movie",
+            "Third Cover Movie",
+            "Fourth Cover Movie"});
+            this.comboBox1.Location = new System.Drawing.Point(155, 105);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(773, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // urlbxtxt
+            // 
+            this.urlbxtxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.urlbxtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urlbxtxt.Location = new System.Drawing.Point(155, 393);
+            this.urlbxtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.urlbxtxt.Name = "urlbxtxt";
+            this.urlbxtxt.Size = new System.Drawing.Size(773, 26);
+            this.urlbxtxt.TabIndex = 3;
+            // 
+            // updatebtn
+            // 
+            this.updatebtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.updatebtn.BackColor = System.Drawing.Color.Black;
+            this.updatebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updatebtn.ForeColor = System.Drawing.Color.White;
+            this.updatebtn.Location = new System.Drawing.Point(740, 542);
+            this.updatebtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.updatebtn.Name = "updatebtn";
+            this.updatebtn.Size = new System.Drawing.Size(189, 34);
+            this.updatebtn.TabIndex = 4;
+            this.updatebtn.Text = "Güncelle";
+            this.updatebtn.UseVisualStyleBackColor = false;
+            this.updatebtn.Click += new System.EventHandler(this.updatebtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(155, 446);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // homemanager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1005, 697);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "homemanager";
+            this.Text = "homemanager";
+            this.Load += new System.EventHandler(this.homemanager_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox urlbxtxt;
+        private System.Windows.Forms.Button updatebtn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox namebxtxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ MoviesPlus/homemanager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BookMyshow
+{
+    public partial class homemanager : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source='DESKTOP-RPQ1FVH\\SQLEXPRESS';initial catalog= bookshow;integrated Security=true");
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        DataSet ds1 = new DataSet();
+        BindingSource BS = new BindingSource();
+        public homemanager()
+        {
+            InitializeComponent();
+            da.SelectCommand = new SqlCommand("select * from mainpic order by(id) ", cs);
+            ds.Clear();
+            da.Fill(ds);
+            da.SelectCommand = new SqlCommand("select mname from mtable ", cs);
+            ds1.Clear();
+            da.Fill(ds1);
+            namebxtxt.DataSource = ds1.Tables[0];
+            namebxtxt.DisplayMember = "mname";
+
+        }
+
+        private void insertbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                da.UpdateCommand = new SqlCommand("update mainpic set Url=@url, moviename=@moviename where id= " + comboBox1.SelectedIndex.ToString(), cs);
+                da.UpdateCommand.Parameters.Add("moviename", SqlDbType.VarChar).Value = namebxtxt.Text;
+                da.UpdateCommand.Parameters.Add("url", SqlDbType.Text).Value = urlbxtxt.Text;
+                cs.Open();
+                da.UpdateCommand.ExecuteNonQuery();
+                cs.Close();
+                foreach (Form f in Application.OpenForms)
+                {
+                    if (f.Text == "home")
+                    {
+                        f.Close();
+                        break;
+                    }
+                }
+                    
+            }
+            catch
+            {
+                MessageBox.Show("Bir Hata Oluştu. Lütfen Tekrar Deneyin.");
+
+            }
+            
+            da.SelectCommand = new SqlCommand("select * from mainpic order by(id) ", cs);
+            ds.Clear();
+            da.Fill(ds);
+            MessageBox.Show("Güncelleme Başarılı.");
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstbtn_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void previousbtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void nextbtn_Click(object sender, EventArgs e)
+        {
+            
+ 
+        }
+
+        private void lastbtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void Records()
+        {
+            
+        }
+        private void dgupdate()
+        {
+            
+        }
+
+        private void displaybtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void homemanager_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            namebxtxt.Text = ds.Tables[0].Rows[int.Parse(comboBox1.SelectedIndex.ToString())][1].ToString();
+            urlbxtxt.Text = ds.Tables[0].Rows[int.Parse(comboBox1.SelectedIndex.ToString())][2].ToString();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = urlbxtxt.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
