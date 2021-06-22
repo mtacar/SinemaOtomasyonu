79db83d0f4a89fdc864ff8bac8d75c53ba798dac
fkavraz <170290032@firat.edu.tr>
2021-06-22 16:15:31 +0300

    But - CASE-16 - Admin Şehir İşlemleri Sayfası
    Adminin şehir ekleyebildiği sayfa yapılacak.
    
    Şehir İşlemleri sayfasının tasarımı.

---
+++ MoviesPlus/SehirIslemleri.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class SehirIslemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(105, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(105, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SİL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(78, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 303);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(20, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Şehir:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.müşteri;
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(348, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 231);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Şehir İşlemleri";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button4.Location = new System.Drawing.Point(439, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 36);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "ANASAYFA";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // SehirIslemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.GRUP3;
+            this.ClientSize = new System.Drawing.Size(640, 332);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SehirIslemleri";
+            this.Text = "SehirIslemleri";
+            this.Load += new System.EventHandler(this.SehirIslemleri_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ MoviesPlus/SehirIslemleri.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class SehirIslemleri : Form
+    {
+        public SehirIslemleri()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=;Initial Catalog=SinemaOtomasyonu;Integrated Security=SSPI");
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+
+
+        int id1;
+        int id2;
+        private void kayitGetirSehir()
+        {
+            //Dil DatagridWiewine Ver Çekme
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.CommandText = "select * from Sehirler";
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+
+            //Dil DatagridWiewine Ver Çekme
+        }
+
+        //FORM YÜKLENDİĞİNDE ŞEHİRLER COMBOBAXA YÜKLENSİN
+        private void SehirIslemleri_Load(object sender, EventArgs e)
+        {
+            kayitGetirSehir();
+
+
+            DataGridViewColumn column;
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                column = dataGridView1.Columns[i];
+                if (i == 0)
+                {
+                    column.Width = 35;
+                }
+                else
+                    column.Width = 95;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //ŞEHİR EKLEME İŞLEMLERİ
+                if (textBox1.Text != "")
+                {
+
+                    cmd = new SqlCommand();
+                    con.Open();
+                    cmd.CommandText = "insert into Sehirler(sehirAd) values (@ad)";
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@ad", textBox1.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    kayitGetirSehir();
+                    MessageBox.Show("Kayıt Eklendi..", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                    MessageBox.Show("İsim Girmediniz..", "Dikkat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //ŞEHİR EKLEME İŞLEMLERİ
+            }
+            catch (Exception)
+            {
+                con.Close();
+                MessageBox.Show("HATA", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //ŞEHİR SİLME İŞLEMLERİ
+                if (textBox1.Text != "")
+                {
+                    if (Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value) == id1)
+                    {
+                        cmd = new SqlCommand();
+                        con.Open();
+                        cmd.CommandText = "delete from Sehirler where sehir_id='" + id1 + "'";
+                        cmd.Connection = con;
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        kayitGetirSehir();
+                        MessageBox.Show("Kayıt Silindi..", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                        MessageBox.Show("Kayıt Silinemedi", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                    MessageBox.Show("Silinecek Kaydı seçiniz..", "Dikkat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //ŞEHİR SİLME İŞLEMLERİ
+            }
+            catch (Exception)
+            {
+
+                con.Close();
+                MessageBox.Show("HATA", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //GÜNCELLEME İŞLEMLERİ
+            try
+            {
+                if (id1 == 0 || id1 == -1)
+                {
+                    MessageBox.Show("Güncellenecek Kaydı Seçiniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (textBox1.Text != "")
+                    {
+
+                        cmd = new SqlCommand();
+                        con.Open();
+                        cmd.CommandText = "update Sehirler set sehirAd='" + textBox1.Text + "' where sehir_id='" + id1 + "'";
+                        cmd.Connection = con;
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        kayitGetirSehir();
+                        MessageBox.Show("Kayıt Güncellendi..", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                        MessageBox.Show("Güncellenecek Kaydı seçiniz..", "Dikkat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+                //GÜNCELLEME İŞLEMLERİ
+            }
+            catch (Exception)
+            {
+
+                con.Close();
+                MessageBox.Show("HATA", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+           
+        }
+
+
+        //DATAGRİDVİEWDEKİ TIKLANAN KAYDIN ID Sİ ALINIYOR
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            id1 = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+        }
+
+
+        //ANASAYFAYA YÖNLENDİRİYOR
+        private void button4_Click(object sender, EventArgs e)
+        {
+            YoneticiIslemleri yi = new YoneticiIslemleri();
+            yi.Show();
+        }
+    }
+}
