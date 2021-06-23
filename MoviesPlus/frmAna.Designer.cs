f369d7463c93e211817b67297ac7dab1201c932d
mtacar <kavrazfurkan99@gmail.com>
2021-06-23 17:20:17 +0300

    But - CASE-7 - Kafe Sistemi Ana Sayfası
    Kafedeki masaların yer aldığı ve bu masaların dolu mu boş mu olduğunu gösteren arayüzün oluşturulması. Ürün,Raporlar ve Kullanıcı menülerinin yapılması.
    
    Kafe Sistemi Ana Sayfası tasarımı.

---
+++ MoviesPlus/frmAna.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    partial class frmAna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAna));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ürünSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ürünEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ürünDüzenleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raporlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.satışlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kullanıcıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniKullanıcıOluşturToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.şifreDeğiştirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayarlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lstMasa = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.masayiKapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.raporlarToolStripMenuItem,
+            this.kullanıcıToolStripMenuItem,
+            this.ayarlarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(1048, 35);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ürünSilToolStripMenuItem,
+            this.ürünEkleToolStripMenuItem,
+            this.ürünDüzenleToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(47, 31);
+            this.toolStripMenuItem1.Text = "Ürün";
+            // 
+            // ürünSilToolStripMenuItem
+            // 
+            this.ürünSilToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.ürünSilToolStripMenuItem.Name = "ürünSilToolStripMenuItem";
+            this.ürünSilToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.ürünSilToolStripMenuItem.Text = "Ürünleri Göster";
+            this.ürünSilToolStripMenuItem.Click += new System.EventHandler(this.ürünSilToolStripMenuItem_Click);
+            // 
+            // ürünEkleToolStripMenuItem
+            // 
+            this.ürünEkleToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.ürünEkleToolStripMenuItem.Name = "ürünEkleToolStripMenuItem";
+            this.ürünEkleToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.ürünEkleToolStripMenuItem.Text = "Ürün Ekle";
+            this.ürünEkleToolStripMenuItem.Click += new System.EventHandler(this.ürünEkleToolStripMenuItem_Click);
+            // 
+            // ürünDüzenleToolStripMenuItem
+            // 
+            this.ürünDüzenleToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.ürünDüzenleToolStripMenuItem.Name = "ürünDüzenleToolStripMenuItem";
+            this.ürünDüzenleToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.ürünDüzenleToolStripMenuItem.Text = "Ürün Düzenle";
+            this.ürünDüzenleToolStripMenuItem.Click += new System.EventHandler(this.ürünDüzenleToolStripMenuItem_Click);
+            // 
+            // raporlarToolStripMenuItem
+            // 
+            this.raporlarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.satışlarToolStripMenuItem});
+            this.raporlarToolStripMenuItem.Name = "raporlarToolStripMenuItem";
+            this.raporlarToolStripMenuItem.Size = new System.Drawing.Size(69, 31);
+            this.raporlarToolStripMenuItem.Text = "Raporlar";
+            // 
+            // satışlarToolStripMenuItem
+            // 
+            this.satışlarToolStripMenuItem.Name = "satışlarToolStripMenuItem";
+            this.satışlarToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.satışlarToolStripMenuItem.Text = "Satışlar";
+            this.satışlarToolStripMenuItem.Click += new System.EventHandler(this.satışlarToolStripMenuItem_Click);
+            // 
+            // kullanıcıToolStripMenuItem
+            // 
+            this.kullanıcıToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniKullanıcıOluşturToolStripMenuItem,
+            this.şifreDeğiştirToolStripMenuItem});
+            this.kullanıcıToolStripMenuItem.Name = "kullanıcıToolStripMenuItem";
+            this.kullanıcıToolStripMenuItem.Size = new System.Drawing.Size(66, 31);
+            this.kullanıcıToolStripMenuItem.Text = "Kullanıcı";
+            // 
+            // yeniKullanıcıOluşturToolStripMenuItem
+            // 
+            this.yeniKullanıcıOluşturToolStripMenuItem.Name = "yeniKullanıcıOluşturToolStripMenuItem";
+            this.yeniKullanıcıOluşturToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.yeniKullanıcıOluşturToolStripMenuItem.Text = "Yeni Kullanıcı Oluştur";
+            this.yeniKullanıcıOluşturToolStripMenuItem.Click += new System.EventHandler(this.yeniKullanıcıOluşturToolStripMenuItem_Click);
+            // 
+            // şifreDeğiştirToolStripMenuItem
+            // 
+            this.şifreDeğiştirToolStripMenuItem.Name = "şifreDeğiştirToolStripMenuItem";
+            this.şifreDeğiştirToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.şifreDeğiştirToolStripMenuItem.Text = "Şifre Değiştir";
+            this.şifreDeğiştirToolStripMenuItem.Click += new System.EventHandler(this.şifreDeğiştirToolStripMenuItem_Click);
+            // 
+            // ayarlarToolStripMenuItem
+            // 
+            this.ayarlarToolStripMenuItem.Name = "ayarlarToolStripMenuItem";
+            this.ayarlarToolStripMenuItem.Size = new System.Drawing.Size(12, 31);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "bosMasa.png");
+            this.imageList1.Images.SetKeyName(1, "doluMasa.png");
+            // 
+            // lstMasa
+            // 
+            this.lstMasa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstMasa.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstMasa.LargeImageList = this.imageList1;
+            this.lstMasa.Location = new System.Drawing.Point(3, 16);
+            this.lstMasa.MultiSelect = false;
+            this.lstMasa.Name = "lstMasa";
+            this.lstMasa.Size = new System.Drawing.Size(1042, 679);
+            this.lstMasa.TabIndex = 1;
+            this.lstMasa.UseCompatibleStateImageBehavior = false;
+            this.lstMasa.SelectedIndexChanged += new System.EventHandler(this.lstMasa_SelectedIndexChanged);
+            this.lstMasa.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            this.lstMasa.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lstMasa_MouseClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstMasa);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1048, 698);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Masalar";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.masayiKapatToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(145, 26);
+            // 
+            // masayiKapatToolStripMenuItem
+            // 
+            this.masayiKapatToolStripMenuItem.Name = "masayiKapatToolStripMenuItem";
+            this.masayiKapatToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.masayiKapatToolStripMenuItem.Text = "Masayi Kapat";
+            this.masayiKapatToolStripMenuItem.Click += new System.EventHandler(this.masayiKapatToolStripMenuItem_Click);
+            // 
+            // frmAna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1048, 733);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SRBST Kafe Otomasyonu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmAna_FormClosing);
+            this.Load += new System.EventHandler(this.frmAna_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStripMenuItem ürünEkleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ürünDüzenleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ürünSilToolStripMenuItem;
+        public System.Windows.Forms.ListView lstMasa;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem masayiKapatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem raporlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem satışlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayarlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kullanıcıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniKullanıcıOluşturToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem şifreDeğiştirToolStripMenuItem;
+    }
+}
+
---
+++ MoviesPlus/frmAna.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    public partial class frmAna : Form
+    {
+        public frmAna()
+        {
+            InitializeComponent();
+        }
+
+
+        public static int MasaNumarasi;
+        public static int masaSayisi;
+        public static Boolean ayaraGit;
+        int doluMasaSayisi = 0;
+        public static frmAna anaFrm;
+
+
+
+
+
+        private void frmAna_Load(object sender, EventArgs e)
+        {
+
+            anaFrm = this;
+
+            DataTable dt = Veritabani.VeriGetir("select * from Ayar");
+
+            DataTable masa = Veritabani.VeriGetir("select MasaSayisi from Ayar");
+            for (int i = 0; i < int.Parse(masa.Rows[0]["MasaSayisi"].ToString()); i++)
+            {
+
+                if (kontroller.veriVarmi("select * from Siparis where MasaNo=" + (i + 1) + " AND Hesap=True"))
+                {
+                    lstMasa.Items.Add((i + 1) + ".Masa");
+                    lstMasa.Items[i].ImageKey = "doluMasa.png";
+                    doluMasaSayisi++;
+                }
+                else
+                {
+                    lstMasa.Items.Add((i + 1) + ".Masa");
+                    lstMasa.Items[i].ImageKey = "bosMasa.png";
+                }
+
+            }
+            masaSayisi = Convert.ToInt32(masa.Rows[0]["MasaSayisi"].ToString());
+
+
+            //    frmGiris frmg = new frmGiris();
+            //   frmg.ShowDialog();
+        }
+
+
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                MasaNumarasi = (int.Parse(lstMasa.SelectedIndices[0].ToString()) + 1);
+
+
+                frmMasa frmMasa = new frmMasa();
+                frmMasa.ShowDialog();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void ürünEkleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmUrunEkle frmUrunEkle = new frmUrunEkle();
+            frmUrunEkle.ShowDialog();
+        }
+
+        private void ürünDüzenleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmUrunDuzenle frm = new frmUrunDuzenle();
+            frm.ShowDialog();
+        }
+
+        private void ürünSilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmUrunGoster frmUrunGoster = new frmUrunGoster();
+            frmUrunGoster.ShowDialog();
+        }
+
+
+
+
+        public void lstMasa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstMasa.SelectedItems.Count > 0)
+            {
+                int masaNo = Convert.ToInt32(lstMasa.SelectedItems[0].Text.Substring(0, lstMasa.SelectedItems[0].Text.IndexOf('.')));
+
+            }
+
+
+        }
+
+        private void lstMasa_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (lstMasa.FocusedItem.Bounds.Contains(e.Location) == true)
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private void masayiKapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (lstMasa.SelectedItems.Count > 0 && lstMasa.SelectedItems[0].ImageKey == "doluMasa.png")
+            {
+                int masaNo = Convert.ToInt32(lstMasa.SelectedItems[0].Text.Substring(0, lstMasa.SelectedItems[0].Text.IndexOf('.')));
+                int siparisNo = Siparis.siparisNoGetir(masaNo);
+                if (MessageBox.Show("Eğer masayı kapatırsanız hesap ödenmeden masa içerisindeki ürünler kaldırılacaktır." +
+                    "\n(*Raporlarda kapatılan masaların bilgilerini göremezsiniz.)" +
+                    "\nDevam etmek istiyor musunuz?", "Uyarı", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    if (Siparis.siparisUrunTemizle(siparisNo))
+                    {
+
+                        if (Siparis.siparisTemizle(siparisNo))
+                        {
+                            lstMasa.SelectedItems[0].ImageKey = "bosMasa.png";
+                            frmAna.anaFrm.lstMasa_SelectedIndexChanged(null, null);
+                        }
+                    }
+                }
+            }
+        }
+        private void satışlarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmSatislar frm = new frmSatislar();
+            frm.ShowDialog();
+        }       
+        
+              
+        private void yeniKullanıcıOluşturToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmKullaniciEkle frm = new frmKullaniciEkle();
+            frm.ShowDialog();
+        }
+
+        private void şifreDeğiştirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmSifreDegistir frm = new frmSifreDegistir();
+            frm.ShowDialog();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void frmAna_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+}
