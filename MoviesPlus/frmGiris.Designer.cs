6af146451736b6c9b33d079f3083d8c729c90446
fkavraz <170290032@firat.edu.tr>
2021-06-23 12:30:29 +0300

    But - CASE-6 - Kafe Sistemine Giriş Sayfası
    Sinema salonuna erken giden, izlediği filmi bitiren ve film öncesi bir takım yiyecek içecek almak isteyen ve oturmak isteyen müşteriler için salonda yer alan sinemaya ait bir kafe bölümü bulunmaktadır.
    
    Kafe arayüzüne giriş sayfası yapılacak.
    Kullanıcı adı ve şifre bölümünden oluşacak.
    
    Kafe Sistemine Giriş sayfası tasarımı.

---
+++ MoviesPlus/frmGiris.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    partial class frmGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGiris));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnGiris);
+            this.panel1.Controls.Add(this.txtSifre);
+            this.panel1.Controls.Add(this.txtKullaniciAdi);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(45, 123);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 153);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.Color.White;
+            this.btnGiris.FlatAppearance.BorderSize = 0;
+            this.btnGiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGiris.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGiris.Image = global::SRBST_Kafe_Otomasyonu.Properties.Resources.giris;
+            this.btnGiris.Location = new System.Drawing.Point(145, 79);
+            this.btnGiris.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(70, 70);
+            this.btnGiris.TabIndex = 4;
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(99, 48);
+            this.txtSifre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(116, 27);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(99, 17);
+            this.txtKullaniciAdi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(116, 27);
+            this.txtKullaniciAdi.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-4, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.FlatAppearance.BorderSize = 2;
+            this.btnKapat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKapat.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKapat.Image = ((System.Drawing.Image)(resources.GetObject("btnKapat.Image")));
+            this.btnKapat.Location = new System.Drawing.Point(305, 1);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(40, 40);
+            this.btnKapat.TabIndex = 2;
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SRBST_Kafe_Otomasyonu.Properties.Resources.logo___yazili;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmGiris
+            // 
+            this.AcceptButton = this.btnGiris;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(347, 287);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kafe Restorant Yönetim Sistemi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmGiris_FormClosing);
+            this.Load += new System.EventHandler(this.frmGiris_Load);
+            this.Resize += new System.EventHandler(this.frmGiris_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ MoviesPlus/frmGiris.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// admin 
+// 123
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+
+    
+    public partial class frmGiris : Form
+    {
+        public static bool durum;
+        public static string kulAdi;
+        public frmGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGiris_Resize(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        { 
+            if (txtKullaniciAdi.Text=="")
+            {
+                MessageBox.Show("Alanları Boş Geçmeyiniz!");
+            }
+            else if (txtSifre.Text == "")
+            {
+                 MessageBox.Show("Şifreyi Boş Geçmeyiniz!");
+            }
+            else
+            {
+                if (kontroller.kullaniciKontrolu(txtKullaniciAdi.Text,Islemler.MD5eDonustur(txtSifre.Text)) == false)
+                {
+                    MessageBox.Show("Kullanıcı Adı veya Şifre Yanlış!");
+                }
+                else
+                {
+                    durum = true;
+                    kulAdi = txtKullaniciAdi.Text;
+                    //this.Close();
+                     frmAna frmAna = new frmAna();
+                     frmAna.ShowDialog();
+                   
+                }
+            }
+           
+            
+            
+        }
+
+        private void frmGiris_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!durum)
+            {
+                Application.Exit(); //prog durdurmak için
+            }
+        }
+
+        private void frmGiris_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //prog durdurmak için
+        }   
+    }
+}
