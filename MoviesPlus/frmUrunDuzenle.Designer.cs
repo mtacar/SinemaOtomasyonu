17155529de6a7efac2ce78ce9f8dbecb4c1d4f10
fkavraz <170290032@firat.edu.tr>
2021-06-24 14:50:40 +0300

    But - CASE-9 - Ürün Düzenle Sayfası
    Ana sayfada yer alan ürünler menüsüne tıklandığında açılacak olan ürün düzenle seçeneği sayfasının oluşturulması.
    Sırasıyla ürünün
    Adı
    Fiyatı
    Grubu
    Resmi
    "Gözat" butonu
    "Düzenle" butonu ve
    "Ürünü Sil" butonu yer alacak.
    
    Ürün düzenle sayfasının tasarımı.

---
+++ MoviesPlus/frmUrunDuzenle.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    partial class frmUrunDuzenle
+    {
+        /// <summary>
+        /// Required designer variable. 
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUrunDuzenle));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnUrunSil = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnDuzenle = new System.Windows.Forms.Button();
+            this.btnGozat = new System.Windows.Forms.Button();
+            this.lblResimYolu = new System.Windows.Forms.Label();
+            this.lblBildirim = new System.Windows.Forms.Label();
+            this.comboUrunGrubu = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrunFiyati = new System.Windows.Forms.TextBox();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(23, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(374, 433);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(48, 48);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUrunSil);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.btnDuzenle);
+            this.groupBox1.Controls.Add(this.btnGozat);
+            this.groupBox1.Controls.Add(this.lblResimYolu);
+            this.groupBox1.Controls.Add(this.lblBildirim);
+            this.groupBox1.Controls.Add(this.comboUrunGrubu);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtUrunFiyati);
+            this.groupBox1.Controls.Add(this.txtUrunAdi);
+            this.groupBox1.Location = new System.Drawing.Point(421, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 250);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ürünün";
+            // 
+            // btnUrunSil
+            // 
+            this.btnUrunSil.Location = new System.Drawing.Point(159, 159);
+            this.btnUrunSil.Name = "btnUrunSil";
+            this.btnUrunSil.Size = new System.Drawing.Size(75, 40);
+            this.btnUrunSil.TabIndex = 16;
+            this.btnUrunSil.Text = "Ürünü Sil";
+            this.btnUrunSil.UseVisualStyleBackColor = true;
+            this.btnUrunSil.Click += new System.EventHandler(this.btnUrunSil_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(215, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 23);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "₺";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(317, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 122);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.Image = ((System.Drawing.Image)(resources.GetObject("btnDuzenle.Image")));
+            this.btnDuzenle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDuzenle.Location = new System.Drawing.Point(31, 159);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(121, 40);
+            this.btnDuzenle.TabIndex = 4;
+            this.btnDuzenle.Text = "Düzenle";
+            this.btnDuzenle.UseVisualStyleBackColor = true;
+            this.btnDuzenle.Click += new System.EventHandler(this.btnDuzenle_Click);
+            // 
+            // btnGozat
+            // 
+            this.btnGozat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGozat.ImageKey = "(none)";
+            this.btnGozat.Location = new System.Drawing.Point(317, 147);
+            this.btnGozat.Name = "btnGozat";
+            this.btnGozat.Size = new System.Drawing.Size(100, 40);
+            this.btnGozat.TabIndex = 3;
+            this.btnGozat.Text = "Gözat";
+            this.btnGozat.UseVisualStyleBackColor = true;
+            this.btnGozat.Click += new System.EventHandler(this.btnGozat_Click);
+            // 
+            // lblResimYolu
+            // 
+            this.lblResimYolu.Location = new System.Drawing.Point(64, 118);
+            this.lblResimYolu.Name = "lblResimYolu";
+            this.lblResimYolu.Size = new System.Drawing.Size(151, 21);
+            this.lblResimYolu.TabIndex = 4;
+            // 
+            // lblBildirim
+            // 
+            this.lblBildirim.AutoSize = true;
+            this.lblBildirim.ForeColor = System.Drawing.Color.OliveDrab;
+            this.lblBildirim.Location = new System.Drawing.Point(28, 202);
+            this.lblBildirim.Name = "lblBildirim";
+            this.lblBildirim.Size = new System.Drawing.Size(0, 13);
+            this.lblBildirim.TabIndex = 3;
+            // 
+            // comboUrunGrubu
+            // 
+            this.comboUrunGrubu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboUrunGrubu.FormattingEnabled = true;
+            this.comboUrunGrubu.Location = new System.Drawing.Point(67, 91);
+            this.comboUrunGrubu.Name = "comboUrunGrubu";
+            this.comboUrunGrubu.Size = new System.Drawing.Size(148, 21);
+            this.comboUrunGrubu.TabIndex = 2;
+            this.comboUrunGrubu.SelectedIndexChanged += new System.EventHandler(this.comboUrunGrubu_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Resmi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Grubu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fiyatı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Adı";
+            // 
+            // txtUrunFiyati
+            // 
+            this.txtUrunFiyati.Location = new System.Drawing.Point(67, 66);
+            this.txtUrunFiyati.Name = "txtUrunFiyati";
+            this.txtUrunFiyati.Size = new System.Drawing.Size(148, 20);
+            this.txtUrunFiyati.TabIndex = 1;
+            this.txtUrunFiyati.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtUrunFiyati.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUrunFiyati_KeyPress);
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtUrunAdi.Location = new System.Drawing.Point(67, 40);
+            this.txtUrunAdi.MaxLength = 50;
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(148, 20);
+            this.txtUrunAdi.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmUrunDuzenle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(873, 457);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmUrunDuzenle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ürün Düzenle";
+            this.Load += new System.EventHandler(this.frmUrunDuzenle_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrunFiyati;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.ComboBox comboUrunGrubu;
+        private System.Windows.Forms.Label lblBildirim;
+        private System.Windows.Forms.Label lblResimYolu;
+        private System.Windows.Forms.Button btnGozat;
+        private System.Windows.Forms.Button btnDuzenle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUrunSil;
+    }
+}
---
+++ MoviesPlus/frmUrunDuzenle.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    public partial class frmUrunDuzenle : Form
+    {
+        public frmUrunDuzenle()
+        {
+            InitializeComponent();
+        }
+        #region Degiskenler
+
+        int urunNo = 0;
+        object comboValue;
+
+        #endregion
+
+        public int seciliUrunNo;
+
+        private void frmUrunDuzenle_Load(object sender, EventArgs e)
+        {
+            
+            comboDoldur();
+            DataTable urun = Veritabani.VeriGetir("select * from Urun where SilinmisMi=False");
+            DataTable grup = Veritabani.VeriGetir("select * from UrunGrubu");
+            for (int groupIndex = 0; groupIndex < grup.Rows.Count; ++groupIndex)
+            {
+                //grubları listviewe ekliyor
+                this.listView1.Groups.Add(grup.Rows[groupIndex]["UrunGrupNo"].ToString(), grup.Rows[groupIndex]["Adi"].ToString());
+
+                for (int urunIndex = 0; urunIndex < urun.Rows.Count; ++urunIndex)
+                {
+                    if (urun.Rows[urunIndex]["UrunGrupNo"].ToString() == grup.Rows[groupIndex]["UrunGrupNo"].ToString())
+                    {
+
+
+                        if (File.Exists(urun.Rows[urunIndex]["ResimYolu"].ToString()))//resimyolunda resim var mı diye kontrol ediyoruz
+                        {
+                            
+                                Image myImage = Image.FromFile(urun.Rows[urunIndex]["ResimYolu"].ToString());//resimi alıyoruz
+                                imageList1.Images.Add(urun.Rows[urunIndex]["UrunNo"].ToString(), myImage);//resmi imageliste ekliyoruz
+                                ListViewItem item = new ListViewItem(urun.Rows[urunIndex]["Adi"].ToString(),
+                                    urun.Rows[urunIndex]["UrunNo"].ToString(), this.listView1.Groups[groupIndex]);//Ürünü,resmi,grubu bağlıyoruz
+                                this.listView1.Items.Insert(0, item);//ürünü ekliyor
+                                // this.listView1.Groups[groupIndex].Items.Insert(0, item);//ürünü gruba ekliyor
+                            
+
+                        }
+                        else //resimyolunda resim yoksa imagekey yinede ürünNo dan oluşturulur. imagekeyden de ürün bilgilerini çekeceğiz.
+                        {
+
+                            ListViewItem item = new ListViewItem(urun.Rows[urunIndex]["Adi"].ToString(), urun.Rows[urunIndex]["UrunNo"].ToString(),
+                            this.listView1.Groups[groupIndex]);//Ürünü,resmi,grubu bağlıyoruz
+                            this.listView1.Items.Insert(0, item);//ürünü ekliyor
+                            //this.listView1.Groups[groupIndex].Items.Insert(0, item);//ürünü gruba ekliyor
+
+                        }
+
+                    }
+
+                }
+            }
+
+            if (seciliUrunNo >0)
+            {
+                listView1.Items[seciliUrunNo - 1].Selected = true;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            if (listView1.SelectedItems.Count > 0)
+                urunNo = Convert.ToInt32(listView1.SelectedItems[0].ImageKey);
+
+            DataTable urunDT = Veritabani.VeriGetir("select * from Urun where UrunNo=" + urunNo + "");
+            txtUrunAdi.Text = urunDT.Rows[0]["Adi"].ToString();
+            txtUrunFiyati.Text = urunDT.Rows[0]["Fiyati"].ToString();
+            lblResimYolu.Text = urunDT.Rows[0]["ResimYolu"].ToString();
+
+            comboUrunGrubu.SelectedValue = urunDT.Rows[0]["UrunGrupNo"];
+            if (File.Exists(lblResimYolu.Text))//resimyolunda resim var mı diye kontrol ediyoruz
+            {
+                pictureBox1.Image = Image.FromFile(lblResimYolu.Text);
+                pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            }
+        }
+        private void comboDoldur()
+        {
+            DataTable dt = Veritabani.VeriGetir("select * from UrunGrubu");
+            comboUrunGrubu.DataSource = dt;
+            comboUrunGrubu.DisplayMember = "Adi";
+            comboUrunGrubu.ValueMember = "UrunGrupNo";
+            comboValue = comboUrunGrubu.SelectedValue;
+        }
+        private void temizle()
+        {
+            txtUrunAdi.Clear();
+            txtUrunFiyati.Clear();
+            pictureBox1.Image = null;
+            lblResimYolu.Text = "";
+        }
+        private void btnDuzenle_Click(object sender, EventArgs e)
+        {
+            
+            if (txtUrunAdi.Text == "" || txtUrunFiyati.Text == "" || lblResimYolu.Text == "" || comboUrunGrubu.Text == "")
+            {
+
+                if (txtUrunAdi.Text == "")
+                {
+                    MessageBox.Show("Boş Alan Bırakmayınız.");
+                }
+                if (txtUrunFiyati.Text == "")
+                {
+                    MessageBox.Show("Boş Alan Bırakmayınız.");
+                }
+                if (lblResimYolu.Text == "")
+                {
+                    MessageBox.Show("Lütfen Bir Resim Yolu Seçiniz.");
+                }
+                if (comboUrunGrubu.Text == "")
+                {
+                    MessageBox.Show("Lütfen Grup Seçiniz.");
+                }
+            }
+            else
+            {
+
+                if (Urun.urunDuzenle(urunNo, lblResimYolu.Text, txtUrunFiyati.Text, comboValue, txtUrunAdi.Text))
+                {
+                    temizle();
+                    lblBildirim.ForeColor = Color.Green;
+                    lblBildirim.Text = "Ürün Başarıyla Düzenlendi.";
+                }
+                else
+                {
+
+                    temizle();
+                    lblBildirim.ForeColor = Color.Red;
+                    lblBildirim.Text = "Ürün Düzenlenirken Hata!";
+                }
+
+
+
+
+            }
+            listView1.Items.Clear();
+            frmUrunDuzenle_Load(null, null);
+        }
+
+        private void btnGozat_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Resimi Seçiniz";
+            openFileDialog1.Filter = "Tümü|*.png;*.jpg;*ico;*.gif|png|*.png|jpeg|*.jpg|icon|*ico|gif|*.gif";
+            openFileDialog1.DefaultExt = "jpg";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    System.IO.File.Copy(@openFileDialog1.FileName, @"Web\resim\urunler\" + openFileDialog1.SafeFileName);
+                    lblResimYolu.Text = @"Web\resim\urunler\" + openFileDialog1.SafeFileName;
+                    pictureBox1.Image = Image.FromFile(lblResimYolu.Text);
+                    pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                }
+                catch 
+                {
+                    MessageBox.Show("Aynı ürün resmi mevcut");
+                }
+                
+            }
+        }
+
+        private void comboUrunGrubu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboValue = comboUrunGrubu.SelectedValue;
+        }
+
+        private void txtUrunFiyati_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtUrunFiyati.Text.IndexOf(',') != -1)
+            {
+                if (e.KeyChar == (char)44)
+                {
+                    e.Handled = true;
+                }
+            }
+            kontroller.txtParaKontrol(e);
+        }
+
+        private void btnUrunSil_Click(object sender, EventArgs e)
+        {
+
+            if (txtUrunAdi.Text == "" || txtUrunFiyati.Text == "" || lblResimYolu.Text == "" || comboUrunGrubu.Text == "" || urunNo == 0)
+            {
+                MessageBox.Show("Lütfen ürün seçiniz!","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                DialogResult sonuc = MessageBox.Show("Ürünü silmek istediğinizden emin misiniz?", "Dikkat", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (sonuc == DialogResult.Yes)
+                {
+                    Urun.urunDuzenle(urunNo, true);
+                    temizle();
+                    listView1.Items.Clear();
+                    frmUrunDuzenle_Load(null, null);
+                }
+            }
+                
+           
+        }
+    }
+}
+
