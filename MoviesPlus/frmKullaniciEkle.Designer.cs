7b0e2768464cc309647f3a33134076265e0a92dd
mtacar <kavrazfurkan99@gmail.com>
2021-06-25 11:00:47 +0300

    But - CASE-11 - Yeni Kullanıcı Oluştur Sayfası
    Ana sayfada yer alan kullanıcı menüsüne tıklandığında açılacak olan yeni kullanıcı oluştur seçeneği sayfasının oluşturulması.
    Yeni kullanıcı oluştur sayfasında sırasıyla
    Kullanıcı Adı
    Şifre
    Ad
    Soyad ve
    "Ekle" butonu yer alacak.
    
    Yeni Kullanıcı Oluştur sayfasının tasarımı.

---
+++ MoviesPlus/frmKullaniciEkle.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    partial class frmKullaniciEkle
+    {
+        /// <summary>
+        /// Required designer variable. 
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKullaniciEkle));
+            this.grpKullanici = new System.Windows.Forms.GroupBox();
+            this.lblBildirim = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtKulAdi = new System.Windows.Forms.TextBox();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.grpKullanici.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpKullanici
+            // 
+            this.grpKullanici.Controls.Add(this.lblBildirim);
+            this.grpKullanici.Controls.Add(this.btnEkle);
+            this.grpKullanici.Controls.Add(this.txtKulAdi);
+            this.grpKullanici.Controls.Add(this.txtSoyadi);
+            this.grpKullanici.Controls.Add(this.label4);
+            this.grpKullanici.Controls.Add(this.txtAdi);
+            this.grpKullanici.Controls.Add(this.label5);
+            this.grpKullanici.Controls.Add(this.txtSifre);
+            this.grpKullanici.Controls.Add(this.label6);
+            this.grpKullanici.Controls.Add(this.label7);
+            this.grpKullanici.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpKullanici.Location = new System.Drawing.Point(0, 0);
+            this.grpKullanici.Name = "grpKullanici";
+            this.grpKullanici.Size = new System.Drawing.Size(278, 267);
+            this.grpKullanici.TabIndex = 13;
+            this.grpKullanici.TabStop = false;
+            // 
+            // lblBildirim
+            // 
+            this.lblBildirim.AutoSize = true;
+            this.lblBildirim.Location = new System.Drawing.Point(111, 174);
+            this.lblBildirim.Name = "lblBildirim";
+            this.lblBildirim.Size = new System.Drawing.Size(0, 13);
+            this.lblBildirim.TabIndex = 13;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Image = ((System.Drawing.Image)(resources.GetObject("btnEkle.Image")));
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEkle.Location = new System.Drawing.Point(114, 200);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 40);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtKulAdi
+            // 
+            this.txtKulAdi.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtKulAdi.Location = new System.Drawing.Point(114, 34);
+            this.txtKulAdi.MaxLength = 25;
+            this.txtKulAdi.Name = "txtKulAdi";
+            this.txtKulAdi.Size = new System.Drawing.Size(100, 21);
+            this.txtKulAdi.TabIndex = 0;
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(114, 136);
+            this.txtSoyadi.MaxLength = 25;
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(100, 21);
+            this.txtSoyadi.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kullanıcı Adı :";
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(114, 102);
+            this.txtAdi.MaxLength = 25;
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(100, 21);
+            this.txtAdi.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Şifre :";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(114, 68);
+            this.txtSifre.MaxLength = 255;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(100, 23);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Ad :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 140);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Soyad :";
+            // 
+            // frmKullaniciEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(278, 267);
+            this.Controls.Add(this.grpKullanici);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmKullaniciEkle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kullanıcı Ekle";
+            this.Load += new System.EventHandler(this.frmKullaniciEkle_Load);
+            this.grpKullanici.ResumeLayout(false);
+            this.grpKullanici.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpKullanici;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtKulAdi;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblBildirim;
+    }
+}
---
+++ MoviesPlus/frmKullaniciEkle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    public partial class frmKullaniciEkle : Form
+    {
+        public frmKullaniciEkle()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (txtKulAdi.Text == "" || txtSifre.Text == "" || txtAdi.Text == "" || txtSoyadi.Text == "")
+            {
+                MessageBox.Show("Lütfen boş alan bırakmayınız!");
+            }
+            else
+            {
+                if (kontroller.veriVarmi("select KullaniciAdi from Kullanici where KullaniciAdi='"+txtKulAdi.Text+"'"))
+                {
+                    lblBildirim.ForeColor = Color.Red;
+                    lblBildirim.Text = "Böyle bir kullanıcı var!.";
+                }
+                else
+                {
+                    if (Kullanici.KullaniciEkle(txtKulAdi.Text, Islemler.MD5eDonustur(txtSifre.Text), txtAdi.Text, txtSoyadi.Text))
+                    {
+                        lblBildirim.ForeColor = Color.Green;
+                        lblBildirim.Text = "Başarıyla kullanıcı eklenmiştir.";
+                        txtAdi.Text = "";
+                        txtKulAdi.Text = "";
+                        txtSifre.Text = "";
+                        txtSoyadi.Text = "";
+                    }
+                    else
+                    {
+                        lblBildirim.ForeColor = Color.Red;
+                        lblBildirim.Text = "Kullanıcı eklenemedi!.";
+                    }
+                }
+                
+            }
+            
+        }
+
+        private void frmKullaniciEkle_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
