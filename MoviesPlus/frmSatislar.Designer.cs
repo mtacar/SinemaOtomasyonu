c6f0e136de67f735f9c36c3ff0e21a477ce55253
fkavraz <170290032@firat.edu.tr>
2021-06-25 15:05:09 +0300

    But - CASE-13 - Satışlar Sayfası
    Ana sayfada yer alan raporlar menüsüne tıklandığında açılacak olan satışlar seçeneği sayfasının oluşturulması.
    satışlar sayfasında sırasıyla
    masa numarası
    başlangıç ve bitiş tarihi
    Satışların gösterildiği arayüz
    Satış ürün detay bölümü yer alacak.
    
    Satışlar sayfasının tasarımı.

---
+++ MoviesPlus/frmSatislar.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    partial class frmSatislar
+    {
+        /// <summary>
+        /// Required designer variable. 
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSatislar));
+            this.gridSatislar = new System.Windows.Forms.DataGridView();
+            this.gridSatisDetayUrun = new System.Windows.Forms.DataGridView();
+            this.txtMasaNo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSatislar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSatisDetayUrun)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridSatislar
+            // 
+            this.gridSatislar.AllowUserToAddRows = false;
+            this.gridSatislar.AllowUserToDeleteRows = false;
+            this.gridSatislar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridSatislar.BackgroundColor = System.Drawing.Color.White;
+            this.gridSatislar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridSatislar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridSatislar.Location = new System.Drawing.Point(4, 19);
+            this.gridSatislar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gridSatislar.Name = "gridSatislar";
+            this.gridSatislar.ReadOnly = true;
+            this.gridSatislar.RowHeadersVisible = false;
+            this.gridSatislar.RowHeadersWidth = 51;
+            this.gridSatislar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridSatislar.Size = new System.Drawing.Size(1107, 264);
+            this.gridSatislar.TabIndex = 0;
+            this.gridSatislar.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridSatislar_CellContentClick);
+            // 
+            // gridSatisDetayUrun
+            // 
+            this.gridSatisDetayUrun.AllowUserToAddRows = false;
+            this.gridSatisDetayUrun.AllowUserToDeleteRows = false;
+            this.gridSatisDetayUrun.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridSatisDetayUrun.BackgroundColor = System.Drawing.Color.White;
+            this.gridSatisDetayUrun.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridSatisDetayUrun.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridSatisDetayUrun.Location = new System.Drawing.Point(4, 19);
+            this.gridSatisDetayUrun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gridSatisDetayUrun.Name = "gridSatisDetayUrun";
+            this.gridSatisDetayUrun.ReadOnly = true;
+            this.gridSatisDetayUrun.RowHeadersVisible = false;
+            this.gridSatisDetayUrun.RowHeadersWidth = 51;
+            this.gridSatisDetayUrun.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridSatisDetayUrun.Size = new System.Drawing.Size(1103, 185);
+            this.gridSatisDetayUrun.TabIndex = 1;
+            this.gridSatisDetayUrun.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridSatisDetayUrun_CellContentClick);
+            // 
+            // txtMasaNo
+            // 
+            this.txtMasaNo.Location = new System.Drawing.Point(137, 37);
+            this.txtMasaNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMasaNo.Name = "txtMasaNo";
+            this.txtMasaNo.Size = new System.Drawing.Size(201, 22);
+            this.txtMasaNo.TabIndex = 0;
+            this.txtMasaNo.TextChanged += new System.EventHandler(this.txtMasaNo_TextChanged);
+            this.txtMasaNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMasaNo_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMasaNo);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1115, 134);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arama Filtre";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(832, 37);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker2.TabIndex = 5;
+            this.dateTimePicker2.Value = new System.DateTime(2014, 5, 11, 23, 59, 0, 0);
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(476, 37);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.Value = new System.DateTime(2014, 5, 11, 6, 15, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(751, 37);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bitiş Tarihi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Başlangıç Tarihi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Masa Numarası";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1399586820_print.png");
+            this.imageList1.Images.SetKeyName(1, "1399590861_adept_preview.png");
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.gridSatislar);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 134);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(1115, 287);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Satışlar";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.gridSatisDetayUrun);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox3.Location = new System.Drawing.Point(0, 421);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(1111, 208);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Satış Ürün Detay";
+            // 
+            // frmSatislar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1115, 629);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmSatislar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Satış Raporu";
+            this.Load += new System.EventHandler(this.frmSatislar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridSatislar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSatisDetayUrun)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridSatislar;
+        private System.Windows.Forms.DataGridView gridSatisDetayUrun;
+        private System.Windows.Forms.TextBox txtMasaNo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ MoviesPlus/frmSatislar.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    public partial class frmSatislar : Form
+    {
+        public frmSatislar()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void frmSatislar_Load(object sender, EventArgs e)
+        {
+            gridSatislar.DataSource = Veritabani.VeriGetir("select * from Satislar");
+            gridSatislar.Columns[4].Visible = false;
+            gridSatislar.Columns[5].Visible =false;
+
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy/MM/dd HH:mm";
+
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.CustomFormat = "yyyy/MM/dd HH:mm";
+
+            dateTimePicker1.Value = DateTime.Now.AddDays(-1);
+            dateTimePicker2.Value = DateTime.Now;
+
+            dateTimePicker1_ValueChanged(null, null);
+        }
+
+
+        private void txtMasaNo_TextChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1_ValueChanged(null, null);
+        }
+
+    
+
+        private void txtMasaNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            kontroller.txtSayiKontrol(e);
+        }
+        private void gridSatislar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            gridSatisDetayUrun.DataSource = Veritabani.VeriGetir("select * from SatisDetayUrun where SiparisNo = " + gridSatislar.SelectedRows[0].Cells[5].Value + "");
+            
+
+            gridSatisDetayUrun.Columns[0].Visible = false;
+            gridSatisDetayUrun.Columns[1].Visible = false;
+
+          
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            if (txtMasaNo.Text == "")
+            {
+                DataTable dt = Veritabani.VeriGetir("select * from Satislar where Tarih >= #" + dateTimePicker1.Text + "# and Tarih <= #" + dateTimePicker2.Text + "#  ");
+                gridSatislar.DataSource = dt;
+            }
+            else
+            {
+                DataTable dt = Veritabani.VeriGetir("select * from Satislar where [Masa Numarası] = " + txtMasaNo.Text + " and Tarih >= #" + dateTimePicker1.Text + "# and Tarih <= #" + dateTimePicker2.Text + "#  ");
+                gridSatislar.DataSource = dt;
+            }
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1_ValueChanged(null, null);
+        }
+
+        private void gridSatislar_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
