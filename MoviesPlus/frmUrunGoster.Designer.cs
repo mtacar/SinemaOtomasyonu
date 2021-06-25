5708330715c0b4e51a031ff551b4e8ab79ff14cd
fkavraz <170290032@firat.edu.tr>
2021-06-25 17:54:33 +0300

    But - CASE-10 - Ürünleri Göster Sayfası
    Ana sayfada yer alan ürünler menüsüne tıklandığında açılacak olan ürünleri göster seçeneği sayfasının oluşturulması.
    Bütün ürünlerin yer aldığı sayfa oluşturulacak.
    
    Ürünleri Göster sayfasının tasarımı.

---
+++ MoviesPlus/frmUrunGoster.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    partial class frmUrunGoster
+    {
+        /// <summary>
+        /// Required designer variable. 
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUrunGoster));
+            this.lstUrun = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ürünüDüzenleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniÜrünEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstUrun
+            // 
+            this.lstUrun.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstUrun.LargeImageList = this.imageList1;
+            this.lstUrun.Location = new System.Drawing.Point(0, 0);
+            this.lstUrun.MultiSelect = false;
+            this.lstUrun.Name = "lstUrun";
+            this.lstUrun.Size = new System.Drawing.Size(784, 561);
+            this.lstUrun.TabIndex = 0;
+            this.lstUrun.UseCompatibleStateImageBehavior = false;
+            this.lstUrun.SelectedIndexChanged += new System.EventHandler(this.lstUrun_SelectedIndexChanged);
+            this.lstUrun.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "logo.png");
+            // 
+            // ürünüDüzenleToolStripMenuItem
+            // 
+            this.ürünüDüzenleToolStripMenuItem.Name = "ürünüDüzenleToolStripMenuItem";
+            this.ürünüDüzenleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ürünüDüzenleToolStripMenuItem.Text = "Ürünü Düzenle";
+            this.ürünüDüzenleToolStripMenuItem.Click += new System.EventHandler(this.ürünüDüzenleToolStripMenuItem_Click);
+            // 
+            // yeniÜrünEkleToolStripMenuItem
+            // 
+            this.yeniÜrünEkleToolStripMenuItem.Name = "yeniÜrünEkleToolStripMenuItem";
+            this.yeniÜrünEkleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.yeniÜrünEkleToolStripMenuItem.Text = "Yeni Ürün Ekle";
+            this.yeniÜrünEkleToolStripMenuItem.Click += new System.EventHandler(this.yeniÜrünEkleToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ürünüDüzenleToolStripMenuItem,
+            this.yeniÜrünEkleToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // frmUrunGoster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lstUrun);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmUrunGoster";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ürünler";
+            this.Load += new System.EventHandler(this.frmUrunGoster_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstUrun;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStripMenuItem ürünüDüzenleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniÜrünEkleToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ MoviesPlus/frmUrunGoster.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    public partial class frmUrunGoster : Form
+    {
+        public frmUrunGoster()
+        {
+            InitializeComponent();
+        }
+        private void urunGoster()
+        {
+            DataTable UrunDT = Veritabani.VeriGetir("select * from Urun where SilinmisMi=False");
+            DataTable urun = Veritabani.VeriGetir("select * from Urun");
+            DataTable grup = Veritabani.VeriGetir("select * from UrunGrubu");
+            for (int groupIndex = 0; groupIndex < grup.Rows.Count; ++groupIndex)
+            {
+                //grubları listviewe ekliyor
+                this.lstUrun.Groups.Add(grup.Rows[groupIndex]["UrunGrupNo"].ToString(), grup.Rows[groupIndex]["Adi"].ToString());
+
+                for (int urunIndex = 0; urunIndex < UrunDT.Rows.Count; ++urunIndex)
+                {
+                    if (UrunDT.Rows[urunIndex]["UrunGrupNo"].ToString() == grup.Rows[groupIndex]["UrunGrupNo"].ToString())
+                    {
+
+
+                        if (File.Exists(UrunDT.Rows[urunIndex]["ResimYolu"].ToString()))//resimyolunda resim var mı diye kontrol ediyoruz
+                        {
+                            Image myImage = Image.FromFile(UrunDT.Rows[urunIndex]["ResimYolu"].ToString());//resimi alıyoruz
+                            imageList1.Images.Add(UrunDT.Rows[urunIndex]["UrunNo"].ToString(), myImage);//resmi imageliste ekliyoruz
+                            ListViewItem item = new ListViewItem(UrunDT.Rows[urunIndex]["Adi"].ToString() + "\n" + UrunDT.Rows[urunIndex]["Fiyati"].ToString() + " ₺",
+                                UrunDT.Rows[urunIndex]["UrunNo"].ToString(), this.lstUrun.Groups[groupIndex]);//Ürünü,resmi,grubu bağlıyoruz
+                            this.lstUrun.Items.Insert(0, item);//ürünü ekliyor
+                            this.lstUrun.Groups[groupIndex].Items.Insert(0, item);//ürünü gruba ekliyor
+                        }
+                        else //resimyolunda resim yoksa imagekey yinede ürünNo dan oluşturulur. imagekeyden de ürün bilgilerini çekeceğiz.
+                        {
+                            ListViewItem item = new ListViewItem(UrunDT.Rows[urunIndex]["Adi"].ToString(), UrunDT.Rows[urunIndex]["UrunNo"].ToString(),
+                            this.lstUrun.Groups[groupIndex]);//Ürünü,resmi,grubu bağlıyoruz
+                            this.lstUrun.Items.Insert(0, item);//ürünü ekliyor
+                            this.lstUrun.Groups[groupIndex].Items.Insert(0, item);//ürünü gruba ekliyor
+                        }
+
+                    }
+
+                }
+            }
+        }
+
+        private void frmUrunGoster_Load(object sender, EventArgs e)
+        {
+           
+           
+            urunGoster();
+
+        }
+
+        private void ürünüDüzenleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmUrunDuzenle frm = new frmUrunDuzenle();
+            frm.seciliUrunNo = seciliUrunNo;
+            frm.ShowDialog();
+        }
+
+        private void yeniÜrünEkleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmUrunEkle frm = new frmUrunEkle();
+            frm.ShowDialog();
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (lstUrun.FocusedItem.Bounds.Contains(e.Location) == true)
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+                }
+            } 
+        }
+
+        public int seciliUrunNo;
+        private void lstUrun_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstUrun.SelectedItems.Count > 0)
+            {
+                seciliUrunNo = int.Parse(lstUrun.SelectedIndices[0].ToString()) + 1;
+            }
+          
+        }
+    }
+}
