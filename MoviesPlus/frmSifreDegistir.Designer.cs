47324770f926ebaf3d852c1bcc894280d15bc293
mtacar <kavrazfurkan99@gmail.com>
2021-06-25 12:32:22 +0300

    But - CASE-12 - Şifre Değiştir Sayfası
    Ana sayfada yer alan kullanıcı menüsüne tıklandığında açılacak olan şifre değiştir seçeneği sayfasının oluşturulması.
    şifre değiştir sayfasında sırasıyla
    eski şifre
    yeni şifre
    yeni şifrenin tekrarı ve
    "Değiştir" butonu yer alacak.
    
    Şifre Değiştir sayfasının tasarımı.

---
+++ MoviesPlus/frmSifreDegistir.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    partial class frmSifreDegistir
+    {
+        /// <summary>
+        /// Required designer variable. 
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSifreDegistir));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEskiSifre = new System.Windows.Forms.TextBox();
+            this.txtYeniSifre = new System.Windows.Forms.TextBox();
+            this.txtYeniSifreTekrar = new System.Windows.Forms.TextBox();
+            this.btnDegistir = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lblBildirim = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eski Şifreniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Yeni Şifreniz:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Yeni Şifreniz(Tekrar):";
+            // 
+            // txtEskiSifre
+            // 
+            this.txtEskiSifre.Location = new System.Drawing.Point(167, 13);
+            this.txtEskiSifre.MaxLength = 255;
+            this.txtEskiSifre.Name = "txtEskiSifre";
+            this.txtEskiSifre.PasswordChar = '*';
+            this.txtEskiSifre.Size = new System.Drawing.Size(100, 21);
+            this.txtEskiSifre.TabIndex = 0;
+            // 
+            // txtYeniSifre
+            // 
+            this.txtYeniSifre.Location = new System.Drawing.Point(167, 41);
+            this.txtYeniSifre.MaxLength = 255;
+            this.txtYeniSifre.Name = "txtYeniSifre";
+            this.txtYeniSifre.PasswordChar = '*';
+            this.txtYeniSifre.Size = new System.Drawing.Size(100, 21);
+            this.txtYeniSifre.TabIndex = 1;
+            // 
+            // txtYeniSifreTekrar
+            // 
+            this.txtYeniSifreTekrar.Location = new System.Drawing.Point(167, 69);
+            this.txtYeniSifreTekrar.MaxLength = 255;
+            this.txtYeniSifreTekrar.Name = "txtYeniSifreTekrar";
+            this.txtYeniSifreTekrar.PasswordChar = '*';
+            this.txtYeniSifreTekrar.Size = new System.Drawing.Size(100, 21);
+            this.txtYeniSifreTekrar.TabIndex = 2;
+            // 
+            // btnDegistir
+            // 
+            this.btnDegistir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDegistir.ImageKey = "HesapOde.png";
+            this.btnDegistir.ImageList = this.imageList1;
+            this.btnDegistir.Location = new System.Drawing.Point(166, 140);
+            this.btnDegistir.Name = "btnDegistir";
+            this.btnDegistir.Size = new System.Drawing.Size(100, 40);
+            this.btnDegistir.TabIndex = 3;
+            this.btnDegistir.Text = "Değiştir";
+            this.btnDegistir.UseVisualStyleBackColor = true;
+            this.btnDegistir.Click += new System.EventHandler(this.btnDegistir_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "HesapOde.png");
+            // 
+            // lblBildirim
+            // 
+            this.lblBildirim.AutoSize = true;
+            this.lblBildirim.Location = new System.Drawing.Point(62, 105);
+            this.lblBildirim.Name = "lblBildirim";
+            this.lblBildirim.Size = new System.Drawing.Size(0, 13);
+            this.lblBildirim.TabIndex = 3;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmSifreDegistir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(328, 209);
+            this.Controls.Add(this.lblBildirim);
+            this.Controls.Add(this.btnDegistir);
+            this.Controls.Add(this.txtYeniSifreTekrar);
+            this.Controls.Add(this.txtYeniSifre);
+            this.Controls.Add(this.txtEskiSifre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmSifreDegistir";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Şifre Değiştir";
+            this.Load += new System.EventHandler(this.frmSifreDegistir_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEskiSifre;
+        private System.Windows.Forms.TextBox txtYeniSifre;
+        private System.Windows.Forms.TextBox txtYeniSifreTekrar;
+        private System.Windows.Forms.Button btnDegistir;
+        private System.Windows.Forms.Label lblBildirim;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ MoviesPlus/frmSifreDegistir.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    public partial class frmSifreDegistir : Form
+    {
+        public frmSifreDegistir()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDegistir_Click(object sender, EventArgs e)
+        {
+            if (txtEskiSifre.Text == "" || txtYeniSifre.Text == "" || txtYeniSifre.Text == "")
+            {
+                errorProvider1.SetError(lblBildirim, "Lütfen boş alan bırakmayınız!");
+            }
+            else
+            {
+                if (!kontroller.kullaniciKontrolu(frmGiris.kulAdi, Islemler.MD5eDonustur(txtEskiSifre.Text)))
+                {
+                    MessageBox.Show("Şifreniz Yanlış!");
+                }
+                else
+                {
+                    if (txtYeniSifre.Text == txtYeniSifreTekrar.Text)
+                    {
+                        if (Kullanici.KullaniciSifreDegistir(frmGiris.kulAdi, Islemler.MD5eDonustur(txtYeniSifre.Text)))
+                        {
+                            lblBildirim.ForeColor = Color.Green;
+                            lblBildirim.Text = "Şifre Değiştirildi.";
+
+                            txtEskiSifre.Text = txtYeniSifre.Text = txtYeniSifreTekrar.Text = "";
+
+                        }
+                        else
+                        {
+                            lblBildirim.ForeColor = Color.Red;
+                            lblBildirim.Text = "Şifre Değiştirilemedi!";
+                        }
+                    }
+                    else
+                    {
+                        lblBildirim.ForeColor = Color.Red;
+                        lblBildirim.Text = "Şifreler Uyumuşmuyor!";
+                    }
+                }
+            }
+        }
+
+        private void frmSifreDegistir_Load(object sender, EventArgs e)
+        {
+            this.Text = frmGiris.kulAdi + " Şifresi Değiştiriliyor";
+        }
+    }
+}
